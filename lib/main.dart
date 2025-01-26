98d83a9ca5cd31296de466144661b799ef12ed10
Curtis Clement <curtis.clement@wonderkind.com>
2025-01-26 14:10:06 +0100

    update version targetting for iOS, fix dotenv.lod missing, and add splash page if connection for auth is loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:study_app/modules/auth/screens/sign_up_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:study_app/modules/auth/screens/sign_up_screen.dart';
+import 'package:study_app/modules/auth/screens/spash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -18,11 +22,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Study App',
-      theme: ThemeData(
-        useMaterial3: true,
-        primarySwatch: Colors.green,
+      theme: ThemeData().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 73, 241, 182),
+        ),
       ),
-      home: SignUpScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          return const SignUpScreen();
+        },
+      ),
     );
   }
 }
