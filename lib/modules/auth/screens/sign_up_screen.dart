8d43791c890a92503733678892fe7a07f4e59f10
Curtis Clement <curtis.clement@wonderkind.com>
2025-01-29 22:42:14 +0100

    add basic sign up ability to application sign up screen

--- lib/modules/auth/screens/sign_up_screen.dart
+++ lib/modules/auth/screens/sign_up_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:study_app/modules/auth/screens/login_screen.dart';
 import 'package:study_app/modules/auth/widgets/sign_up_form.dart';
 import 'package:study_app/modules/auth/widgets/login_buttons.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _auth = FirebaseAuth.instance;
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -16,9 +19,37 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
+  var _isRegistering = false;
 
-  void _onConfirm() {
-    print('SIGN UP');
+  void _onConfirm() async {
+    final isValid = _formKey.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+
+    setState(() {
+      _isRegistering = true;
+    });
+
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+    } catch (error) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Registration failed. Please try again.'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
+    } finally {
+      setState(() {
+        _isRegistering = false;
+      });
+    }
   }
 
   void _onSecondaryButton() {
@@ -47,23 +78,28 @@ class _SignUpScreenState extends State<SignUpScreen> {
             key: _formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Sign Up',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                SignUpForm(
-                  emailController: _emailController,
-                  passwordController: _passwordController,
-                  confirmPasswordController: _confirmPasswordController,
-                ),
-                LoginButtons(
-                  primaryButtonText: 'Sign Up',
-                  secondaryButtonText: 'I already have an account',
-                  onConfirm: _onConfirm,
-                  onSecondaryButton: _onSecondaryButton,
-                ),
-              ],
+              children: _isRegistering
+                  ? [
+                      Text('Registering...'),
+                      CircularProgressIndicator(),
+                    ]
+                  : [
+                      Text(
+                        'Sign Up',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      SignUpForm(
+                        emailController: _emailController,
+                        passwordController: _passwordController,
+                        confirmPasswordController: _confirmPasswordController,
+                      ),
+                      LoginButtons(
+                        primaryButtonText: 'Sign Up',
+                        secondaryButtonText: 'I already have an account',
+                        onConfirm: _onConfirm,
+                        onSecondaryButton: _onSecondaryButton,
+                      ),
+                    ],
             ),
           ),
         ),
